123b7756c151962c0610595b72b40d689eaa9f89
Marcin Zawadzki <mar.zawadzki99@gmail.com>
2024-04-17 14:37:16 +0200

    Added working data base to app

---
+++ SolidAppPlaygroundAPI/SolidAppPlayground/SolidAppPlayground/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolidAppPlayground.Models;
+
+namespace SolidAppPlayground.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ SolidAppPlaygroundAPI/SolidAppPlayground/SolidAppPlayground/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SolidAppPlayground.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string NickName { get; set; }
+    }
+}
